527f24c8643ed3aec76390f6ad489c0f2fd1f82c
Omkar Jahagirdar <omkar3602@gmail.com>
2021-07-07 16:30:46 +0530

    added script on main_page

--- static/scripts/donation_page.js
+++ static/scripts/donation_page.js
@@ -1,8 +1,8 @@
 var counter=1;
-    setInterval(function(){
-        document.getElementById('radio' + counter).checked=true;
-        counter++;
-        if(counter>5){
-            counter=1;
-        }
-    }, 6500);
+setInterval(function(){
+    document.getElementById('radio' + counter).checked=true;
+    counter++;
+    if(counter>5){
+        counter=1;
+    }
+}, 6500);
