e192fc75b787fa59b19774db0d9650c8f0db9298
Omkar Jahagirdar <omkar3602@gmail.com>
2021-07-08 17:52:18 +0530

    privacy done

--- main_page/urls.py
+++ main_page/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='main_page')
+    path('',views.home,name='main_page'),
+    path('privacy_policy/',views.privacy_policy,name='privacy_policy'),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'main_page.html')
+
+def privacy_policy(request):
+    return render(request,'privacy_policy_page.html')
